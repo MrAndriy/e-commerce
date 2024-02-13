d7e21ecdf3f9d466510bfe3e9733c93d41649a03
Andriy Mriy <andriymriy@gmail.com>
2024-02-13 23:34:06 +0200

    feat: landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import { cn } from "@/lib/utils";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +22,9 @@ export default function RootLayout({
       <body
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
-        {children}
+        <main className="relative flex flex-col min-h-screen">
+          <div className="flex-grow flex-1">{children}</div>
+        </main>
       </body>
     </html>
   );
---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import { cn } from "@/lib/utils"
+
+type MaxWidthWrapperProps = {
+  className?: string
+  children: React.ReactNode
+  [key: string]: any
+}
+
+function MaxWidthWrapper({ className, children, ...props }: MaxWidthWrapperProps) {
+  return (
+    <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)} {...props}>
+      {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper

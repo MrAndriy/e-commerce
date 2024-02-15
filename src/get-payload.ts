9a11445e95fcbf6f3e96205c88085bf62ee02516
Andriy Mriy <andriymriy@gmail.com>
2024-02-15 23:35:18 +0200

    feat: trcp, sing-un page

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,18 +1,29 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import payload, { Payload } from 'payload';
-import type { InitOptions } from 'payload/config';
+import dotenv from "dotenv";
+// import nodemailer from "nodemailer";
+import path from "path";
+import payload, { Payload } from "payload";
+import type { InitOptions } from "payload/config";
 
 dotenv.config({
-  path: path.resolve(__dirname, '../.env')
+  path: path.resolve(__dirname, "../.env"),
 });
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.resend.com",
+//   secure: true,
+//   port: 465,
+//   auth: {
+//     user: "resend",
+//     pass: process.env.RESEND_API_KEY,
+//   },
+// });
 
 let cached = (global as any).payload;
 
 if (!cached) {
   cached = (global as any).payload = {
     client: null,
-    promise: null
+    promise: null,
   };
 }
 
@@ -20,9 +31,9 @@ interface Args {
   initOptions?: Partial<InitOptions>;
 }
 
-export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+export const getPayloadClient = async ({ initOptions }: Args = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
-    throw new Error('PAYLOAD_SECRET is not defined');
+    throw new Error("PAYLOAD_SECRET is missing");
   }
 
   if (cached.client) {
@@ -33,12 +44,12 @@ export const getPayloadClient = async ({ initOptions }: Args = {}) => {
     cached.promise = payload.init({
       // email: {
       //   transport: transporter,
-      //   fromAddress: 'hello@joshtriedcoding.com',
-      //   fromName: 'DigitalHippo'
+      //   fromAddress: "hello@joshtriedcoding.com",
+      //   fromName: "DigitalHippo",
       // },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
-      ...(initOptions || {})
+      ...(initOptions || {}),
     });
   }
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
-import express from 'express';
+import { inferAsyncReturnType } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
 
-import { getPayloadClient } from './get-payload';
-import { nextApp, nextHandler } from './next-utils';
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+import { appRouter } from "./trpc";
+
+const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+});
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -12,14 +22,22 @@ const start = async () => {
       express: app,
       onInit: async (cms) => {
         cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
-      }
-    }
+      },
+    },
   });
+
+  app.use(
+    "/api/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    }),
+  );
 
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    payload.logger.info('Next.js started');
+    payload.logger.info("Next.js started");
 
     app.listen(PORT, async () => {
       payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`);
